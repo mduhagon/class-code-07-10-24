0e96366279afbaf6b3bbc0d2195e0eefa484a532
Maria Duhagon <mduhagon@gmail.com>
2024-10-14 20:04:07 +0200

    use XMLHttpRequest async request and handle possible errors

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
 function getBookList() {
     // get the list from the API
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://openlibrary.org/people/mekBot/books/want-to-read.json", false ); // false for synchronous request
-    // because this is a sync request, this line will only be 'done' after the response from 
-    // the API server is finished / came back, and a response is already available.
-    xmlHttp.send( null ); 
+    try {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "https://openlibrary.org/people/mekBot/books/want-to-read.json", true ); // true for asynchronous request
+        // we define the max time to wait for the request to complete: 5 seconds (expressed as milliseconds)
+        xmlHttp.timeout = 5000;
 
-    const response = JSON.parse(xmlHttp.responseText);
-    const listOfBooks = response.reading_log_entries;
+        // this is the event handler for when the request is complete
+        xmlHttp.onreadystatechange = function () {
+            console.log("onreadystatechange is called");
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) { // 4 means the request is done
+                const response = JSON.parse(xmlHttp.responseText);
+                renderBooks(response);
+            } else if (xmlHttp.status === 0) { // a timeout is going to be considered status = 0
+                console.log("xmlHttp.status is 0");
+                displayError();    
+            }    
+        }
+        // this is the event handler for when errors occur
+        xmlHttp.onerror = function () {
+            console.log("onerror is called");
+            displayError();    
+        }    
+
+        //trigger / send the request
+        xmlHttp.send( null );
+    } catch (error) {
+        console.log("catch block is called");
+        displayError();
+    }
+}
+
+// we created this so we do not duplicate these lines in multiple places
+function displayError() {
+    const errorMsgDiv = document.createElement("div");
+    errorMsgDiv.innerHTML = "<h3>Could not load the list of books!</h3>";
+    document.body.appendChild(errorMsgDiv);
+}
+
+function renderBooks(responseJson) {    
+    const listOfBooks = responseJson.reading_log_entries;
 
     console.log(listOfBooks);
 
@@ -24,9 +56,4 @@ function getBookList() {
 
         document.body.appendChild(bookDiv);
     }
-
-    // remove the first 3 books
-    
-    
-
 }
